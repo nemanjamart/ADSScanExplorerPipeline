6868ad895dbe2e05353e3d6fc84b4c70aacec353
mattiasw <mattias.wangblad@gmail.com>
2022-05-04 11:28:55 +0200

    Added task for detecting new volumes

---
+++ ADSScanExplorerPipeline/ingestor.py
@@ -0,0 +1,130 @@
+import re
+import os
+from hashlib import md5
+from typing import Iterable
+from ADSScanExplorerPipeline.models import JournalVolume, Page, Article, PageColor
+from ADSScanExplorerPipeline.app import ADSScanExplorerPipeline
+from sqlalchemy.orm import Session
+from PIL import Image
+from PIL.TiffTags import TAGS
+
+def parse_top_file(file_path: str, journal_volume: JournalVolume, session: Session) -> Iterable[Page]:
+    """
+    Loops through the volumes .top file and yields a Page object for each row
+    """
+    with open(file_path) as file:
+        line_num = 0
+        for line in file:
+            line_num += 1
+            if line_num < 5: #Top file contains 4 header lines
+                continue
+            running_page_num = line_num - 4
+            line_split = re.split("\s+", line)
+            #First is page name, second possibly page number if exists
+            name = line_split[0]
+            page = Page.get_or_create(name, journal_volume, session)
+            page.volume_running_page_num = running_page_num
+            if len(line_split) > 1:
+                page_num = line_split[1]
+                page.label = page_num
+            yield page
+
+def parse_dat_file(file_path: str, journal_volume: JournalVolume, session: Session):
+    """
+    Loops through the volumes .dat file and yields a Article object for each row
+    Each article gets linked with all pages associated to that article
+    """
+    with open(file_path) as file:
+        line_num = 0
+        for line in file:
+            line_num += 1
+            line_split = re.split("[\s+|]", line.strip())
+            article_name = line_split[0]
+            article = Article.get_or_create(article_name, journal_volume, session)
+            article_page_num = 0
+            first = True
+            for page_name in line_split[3:]:
+                if len(page_name) != 11:
+                    continue
+                article_page_num += 1
+                page = Page.get_from_name_and_journal(page_name, journal_volume.id, session)
+                if first:
+                    article.page_start = page.volume_running_page_num
+                    first = False
+                article.pages.append(page)
+            article.page_end = page.volume_running_page_num
+            yield article
+
+def parse_image_files(image_path: str, journal_volume: JournalVolume, session: Session):
+    """
+    Loops through the volumes image files and parse out width and height from the TIFF header
+    Some pages have multiple images a Black-and-White without file ending and a .tif which can
+    be either greyscale or color based on the number of channels.
+    """
+    for filename in os.listdir(image_path):
+        if filename.endswith(".png") or filename.endswith(".jpg"):
+            continue
+        base_filename = filename.replace(".tif", "")
+        page = Page.get_from_name_and_journal(base_filename, journal_volume.id, session)
+        if not page:
+            #Image file not in lists 
+            #TODO possibly log this somewhere
+            continue
+
+        img = Image.open(os.path.join(image_path, filename))
+        meta_dict = {TAGS[key] : img.tag[key] for key in img.tag_v2}
+        width = meta_dict["ImageWidth"][0]
+        height = meta_dict["ImageLength"][0]
+
+        if filename.endswith(".tif"):
+            n_samples = len(meta_dict["BitsPerSample"])
+            #The tiff images are either color if having 3 channels or greyscale if only 1 channel
+            if n_samples > 1:
+                color = PageColor.Color
+            else:
+                color = PageColor.Greyscale
+            page.color_type = color
+            page.width = width
+            page.height = height
+        else:
+            page.width = width
+            page.height = height
+        img.close()
+        yield page
+
+def identify_journals(iput_folder_path : str) -> Iterable[JournalVolume]:
+    for type in os.listdir(iput_folder_path):
+        type_path = os.path.join(iput_folder_path, type)
+        if not os.path.isdir(type_path):
+            continue
+        for journal in os.listdir(type_path):
+            journal_path = os.path.join(type_path, journal) 
+            if not os.path.isdir(journal_path):
+                continue
+            for file in os.listdir(journal_path):
+                if ".top" in file:
+                    volume = parse_volume_from_top_file(file, journal)
+                    vol = JournalVolume(type, journal, volume)
+                    vol.file_hash = hash_volume()
+                    yield vol
+
+def parse_volume_from_top_file(filename : str, journal : str):
+    return filename.replace(".top", "").replace(journal, "")
+
+def hash_volume(base_path: str, vol: JournalVolume):
+    """
+    Calculates a md5 hash from the change dates of all the associated images and files to the volume  
+    """
+    vol_hash = ""
+    list_path = os.path.join(base_path, "lists" ,vol.type, vol.journal)
+    for file in os.listdir(list_path):
+        file_path = os.path.join(list_path, file)
+        modified_time = os.path.getmtime(file_path)
+        vol_hash = md5((vol_hash + str(modified_time)).encode("utf-8")).hexdigest()
+    
+    image_path = os.path.join(base_path, "bitmaps" ,vol.type, vol.journal, vol.volume, "600")
+    for file in os.listdir(image_path):
+        file_path = os.path.join(image_path, file)
+        modified_time = os.path.getmtime(file_path)
+        vol_hash = md5((vol_hash + str(modified_time)).encode("utf-8")).hexdigest()
+    print(vol_hash)
--- ADSScanExplorerPipeline/tasks.py
+++ ADSScanExplorerPipeline/tasks.py
@@ -1,10 +1,10 @@
 
 import os
 import uuid
+from ADSScanExplorerPipeline.ADSScanExplorerPipeline.models import VolumeStatus
 from ADSScanExplorerPipeline.models import JournalVolume
-from ADSScanExplorerPipeline.pages import read_top_file, read_dat_file, read_image_files
+from ADSScanExplorerPipeline.ingestor import parse_top_file, parse_dat_file, parse_image_files, identify_journals
 from kombu import Queue
-from google.protobuf.json_format import MessageToDict
 from datetime import datetime
 import ADSScanExplorerPipeline.app as app_module
 import adsmsg
@@ -16,15 +16,15 @@ app = app_module.ADSScanExplorerPipeline('ads-scan-pipeline', proj_home=proj_hom
 logger = app.logger
 
 app.conf.CELERY_QUEUES = (
-    Queue('process-citation-changes', app.exchange, routing_key='process-citation-changes'),
+    Queue('process-new-volume', app.exchange, routing_key='process-new-volume'),
     Queue('maintenance_reevaluate', app.exchange, routing_key='maintenance_reevaluate'),
     Queue('output-results', app.exchange, routing_key='output-results'),
 )
 
 # ============================= TASKS ============================================= #
 
-@app.task(queue='process-new-volume')
-def task_process_new_volume(journal_volume_id: uuid, force=False):
+@app.task(queue='process-volume')
+def task_process_volume(journal_volume_id: uuid, force=False):
     """
     Process new volume
     """
@@ -33,17 +33,48 @@ def task_process_new_volume(journal_volume_id: uuid, force=False):
         try:
             file_path = "something"
             journal_volume = JournalVolume.get(journal_volume_id, session)
-            read_top_file(file_path, journal_volume, session)
-            read_dat_file(file_path, journal_volume, session)
-            read_image_files(file_path, journal_volume, session)
+            for page in parse_top_file(file_path, journal_volume):
+                session.add(page)
+
+            for article in parse_dat_file(file_path, journal_volume):
+                session.add(article)
+
+            for page in parse_image_files(file_path, journal_volume):
+                session.add(page)
+                
             #TODO copy image files
         except:
             session.rollback()
+            journal_volume.status = VolumeStatus.Error
+            session.add(journal_volume)
+            session.commit()
             raise
         else:
             session.commit()
 
-    
+@app.task(queue='investigate-new-volumes')
+def task_investigate_new_volumes(force=False):
+    """
+    Investigate if any new or updated volumes exists
+    """
+    with app.session_scope() as session:
+        #TODO
+        base_path = ""
+        for vol in identify_journals():
+            existing_vol = JournalVolume.get_from_obj(vol, session)
+            if existing_vol:
+                if vol.file_hash != existing_vol.file_hash:
+                    existing_vol.status = VolumeStatus.Update
+                    existing_vol.file_hase = vol.file_hash
+                    session.add(existing_vol)
+                    session.commit()
+                    #TODO add update task
+            else:
+                vol.status = VolumeStatus.New
+                session.add(vol)
+                session.commit()
+                #TODO add new task
+                
 
 if __name__ == '__main__':
     app.start()
