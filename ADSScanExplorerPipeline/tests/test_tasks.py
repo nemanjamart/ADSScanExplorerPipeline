30953cbe98891f5c667cd765ce9d57195f7670c4
mattiasw <mattias.wangblad@gmail.com>
2022-05-12 13:33:23 +0200

    Added test for uploading files task

--- ADSScanExplorerPipeline/tests/test_tasks.py
+++ ADSScanExplorerPipeline/tests/test_tasks.py
@@ -3,8 +3,11 @@ import os
 import unittest
 from unittest.mock import patch
 from alchemy_mock.mocking import UnifiedAlchemyMagicMock
-from ADSScanExplorerPipeline.tasks import task_investigate_new_volumes, task_process_volume
+from ADSScanExplorerPipeline.tasks import task_investigate_new_volumes, task_process_volume, task_upload_image_files_for_volume
 from ADSScanExplorerPipeline.models import JournalVolume, VolumeStatus, Page, PageColor, PageType, Article
+from moto import mock_s3
+import boto3
+
 class TestModels(unittest.TestCase):
 
     test_home = os.path.realpath(os.path.join(os.path.dirname(__file__), '../'))
@@ -41,12 +44,11 @@ class TestModels(unittest.TestCase):
         session_scope.return_value = session
 
         used_session = task_process_volume(self.data_folder, vol.id)
-        self.assertEqual(len(used_session.query(JournalVolume).filter().all()), 1)
         for vol in used_session.query(JournalVolume).filter(JournalVolume.journal == "").all():
             self.assertEqual(vol.type, "seri")
             self.assertEqual(vol.journal, "test.")
             self.assertEqual(vol.volume, "0001")
-            self.assertEqual(vol.status, VolumeStatus.Done)
+            self.assertEqual(vol.status, VolumeStatus.Db_done)
 
         #Mocked session doesn't update the row but adds a new row when adding to the db therefore we get 3 identical rows
         self.assertEqual(len(used_session.query(Page).filter().all()), 3)
@@ -64,3 +66,37 @@ class TestModels(unittest.TestCase):
         for article in used_session.query(Article).filter(JournalVolume.journal == "").all():
             self.assertEqual(article.bibcode, expected_article.bibcode)
             self.assertEqual(article.journal_volume_id, vol.id)
+
+    @mock_s3
+    @patch('ADSScanExplorerPipeline.app.ADSScanExplorerPipeline.session_scope')
+    @patch('ADSScanExplorerPipeline.models.JournalVolume.get_from_id_or_name')
+    @patch('ADSScanExplorerPipeline.models.Page.get_all_from_volume')
+    @patch('ADSScanExplorerPipeline.models.Page.get_from_name_and_journal')
+    def test_task_upload_image_files_for_volume(self, get_from_name_and_journal, get_all_from_volume, get_from_id_or_name, session_scope):
+        
+        vol = JournalVolume("seri", "test.", "0001")
+        vol.id = '60181735-6f0c-47a6-bf9d-47a1f1fc4fc4'
+        get_from_id_or_name.return_value = vol
+
+        expected_page =  Page("0000255,001", vol.id)
+        get_all_from_volume.return_value = [expected_page]
+        get_from_name_and_journal.return_value = expected_page
+
+        session = UnifiedAlchemyMagicMock()
+        session_scope.return_value = session
+
+        conn = boto3.resource('s3')
+        bucket = conn.create_bucket(Bucket='scan-explorer')
+
+        used_session = task_upload_image_files_for_volume(self.data_folder, vol.id)
+
+        for vol in used_session.query(JournalVolume).filter(JournalVolume.journal == "").all():
+            self.assertEqual(vol.status, VolumeStatus.Bucket_done)
+
+        keys = []
+        for obj in bucket.objects.all():
+            keys.append(obj.key)
+        self.assertEqual(len(keys), 2)
+        self.assertTrue('bitmaps/seri/test_/0001/600/0000255,001' in keys)
+        self.assertTrue('bitmaps/seri/test_/0001/600/0000255,001.tif' in keys)
+
