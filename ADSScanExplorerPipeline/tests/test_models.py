5412ce0ff2644d4cde4252dfbb275231c81117b2
mattiasw <mattias.wangblad@gmail.com>
2022-05-17 12:00:26 +0200

    Added test for page labeling

--- ADSScanExplorerPipeline/tests/test_models.py
+++ ADSScanExplorerPipeline/tests/test_models.py
@@ -1,5 +1,6 @@
 import unittest
-from  ADSScanExplorerPipeline.models import PageType
+from  ADSScanExplorerPipeline.models import PageType, Page
+from  ADSScanExplorerPipeline.exceptions import PageNameException
 
 class TestModels(unittest.TestCase):
 
@@ -9,3 +10,18 @@ class TestModels(unittest.TestCase):
         self.assertEqual(PageType.page_type_from_separator(":"), PageType.BackMatter)
         self.assertEqual(PageType.page_type_from_separator("I"), PageType.Insert)
         self.assertEqual(PageType.page_type_from_separator("P"), PageType.Plate)
+    
+    def testPageLabel(self):
+        page = Page("0000255.000", "vol_id")
+        self.assertEqual(page.label, "255")
+
+        page = Page("0000255.001", "vol_id")
+        self.assertEqual(page.label, "255-1")
+
+        page = Page("A000255.000", "vol_id")
+        self.assertEqual(page.label, "A-255")
+        
+        page = Page("A000255.001", "vol_id")
+        self.assertEqual(page.label, "A-255-1")
+
+        self.assertRaises(PageNameException, Page, "00023232", "")
