3deb03aca06c63406f491b33a9906d87f9392f7d
mattiasw <mattias.wangblad@gmail.com>
2022-05-18 16:55:18 +0200

    Updated setup scripts to be less hardcoded

--- setup_es.py
+++ setup_es.py
@@ -1,10 +1,9 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import ADSScanExplorerPipeline.models
+from elasticsearch import Elasticsearch
+import json
 import argparse
-import os
 from adsputils import setup_logging, load_config
 from distutils.util import strtobool
+import os
 
 # ============================= INITIALIZATION ==================================== #
 
@@ -18,20 +17,22 @@ logger = setup_logging('setup_db.py', proj_home=proj_home,
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-
     parser.add_argument("--re-create",
                     dest="delete",
                     required=False,
                     default="False",
                     type=str,
-                    help="Deletes all existing ads_scan_explorer tables in the DB before creating fresh tables")
+                    help="Deletes existing ads_scan_explorer index before creating a new fresh instance")
     args = parser.parse_args()
-    engine = create_engine(config.get("SQLALCHEMY_URL", ""), echo=False)
-    conn = engine.connect()
-    DBSession = sessionmaker(bind=engine)
-    session = DBSession()
 
-    if bool(strtobool(args.delete)):
-        ADSScanExplorerPipeline.models.Base.metadata.drop_all(engine)
-    ADSScanExplorerPipeline.models.Base.metadata.create_all(engine)
-    session.commit()
+
+es = Elasticsearch(config.get("ELASTIC_SEARCH_URL", ""))
+if bool(strtobool(args.delete)):
+    es.indices.delete(index = config.get("ELASTIC_SEARCH_INDEX", ""))
+
+es_mapping_file = "./es/ocr.json"
+
+with open(es_mapping_file, 'r') as f:
+    index_dict = json.load(f)
+    es.indices.create(index = config.get("ELASTIC_SEARCH_INDEX", ""), mappings = index_dict)
+es.transport.close()
